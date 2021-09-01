f3c3f2fafaf0133eb854ef80caf492f4a0a3708a
arifphero <ahammadarif232@gmail.com>
2021-09-02 00:50:53 +0600

    first commit

---
+++ js/app.js
@@ -0,0 +1,6 @@
+const searchInput = document.getElementById('searchInput');
+const searchButton = document.getElementById('searchButton');
+
+searchButton.addEventListener('click', function(){
+    console.log('hello');
+})
