5a5d95b6bf77405c406b2e61ff177f638de445ac
arifphero <ahammadarif232@gmail.com>
2022-03-01 21:21:59 +0600

    update comments

--- js/app.js
+++ js/app.js
@@ -1,60 +1,59 @@
 // variable declaration
-const inputField = document.getElementById('input-field');
-const searchButton = document.getElementById('search-button');
-const totlaResult = document.getElementById('total-result');
-const errorMessage = document.getElementById('error-message');
-const bookContainer = document.getElementById('book-container');
-
+const inputField = document.getElementById("input-field");
+const searchButton = document.getElementById("search-button");
+const totlaResult = document.getElementById("total-result");
+const errorMessage = document.getElementById("error-message");
+const bookContainer = document.getElementById("book-container");
 
 // add event listener
-    searchButton.addEventListener('click', () => { 
-       
-    ///// preloader added ///// 
-    errorMessage.innerHTML = `
+searchButton.addEventListener("click", () => {
+  ///// preloader added /////
+  errorMessage.innerHTML = `
     <div class="w-100 h-100 d-flex justify-content-center align-items-center ">
     <div class="spinner-border text-info" role="status">
     <span class="visually-hidden">Loading...</span>
     </div>
     </div>
-
     `;
-    const search = inputField.value;
-    const url = `https://openlibrary.org/search.json?q=${search}`;
-    console.log(url);
-    fetch(url)
-    .then(res => res.json())
-    .then(data => showData(data));
+  const search = inputField.value;
+  const url = `https://openlibrary.org/search.json?q=${search}`;
+  console.log(url);
+  fetch(url)
+    .then((res) => res.json())
+    .then((data) => showData(data));
 
-    const showData = (books => {
+  const showData = (books) => {
     const total = books.numFound;
     console.log(books);
 
-        books = books.docs
-        console.log(books);
-        const newArray = books.filter((book) => book.cover_i !== undefined && book.author_name !== undefined && book.publisher !== undefined && book.title !== undefined && book.first_publish_year !== undefined);
-        console.log(newArray);
+    books = books.docs;
+    console.log(books);
+    const newArray = books.filter(
+      (book) =>
+        book.cover_i !== undefined &&
+        book.author_name !== undefined &&
+        book.publisher !== undefined &&
+        book.title !== undefined &&
+        book.first_publish_year !== undefined
+    );
+    console.log(newArray);
 
-         inputField.value = '';
-
+    inputField.value = "";
 
     ///// validation for ERROR MESSAGE /////
-        if ( newArray.length === 0){
-            errorMessage.innerHTML = '';
-            errorMessage.innerHTML = ` NO RESULT FOUND`;
-            totlaResult.innerHTML = '';
-            bookContainer.innerHTML = '';
-        }
-        else{
-           
-            totlaResult.innerHTML = `Total Books: ${total}, Showing ${newArray.length} Books`;
-            errorMessage.innerHTML = '';
-            
+    if (newArray.length === 0) {
+      errorMessage.innerHTML = "";
+      errorMessage.innerHTML = ` NO RESULT FOUND`;
+      totlaResult.innerHTML = "";
+      bookContainer.innerHTML = "";
+    } else {
+      totlaResult.innerHTML = `Total Books: ${total}, Showing ${newArray.length} Books`;
+      errorMessage.innerHTML = "";
 
-    //// forEach loop  /////
-            newArray.forEach(book => {
-            
-                const div = document.createElement('div');
-                div.innerHTML = `
+      //// forEach loop  /////
+      newArray.forEach((book) => {
+        const div = document.createElement("div");
+        div.innerHTML = `
                  <div class = "card my-3" style= "width: 18rem; height: auto">
                  <img class=" card-img-top img-fluid"  style="width:100%; height 250px" src= 'https://covers.openlibrary.org/b/id/${book.cover_i}-M.jpg'>
                  <div class = "card-body">
@@ -65,8 +64,8 @@ const bookContainer = document.getElementById('book-container');
                  </div>
                  </div>
                 `;
-                bookContainer.appendChild(div);
-            })
-        }
-    })
-})
+        bookContainer.appendChild(div);
+      });
+    }
+  };
+});
